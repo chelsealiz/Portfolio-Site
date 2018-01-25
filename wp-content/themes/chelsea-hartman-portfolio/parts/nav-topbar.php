505688489b573e86823a345d5ab59192fab61756
Chelsea Hartman <overstreetchelsea@gmail.com>
2018-01-25 16:07:51 -0500

    Header Setup and Mobile Header

--- wp-content/themes/chelsea-hartman-portfolio/parts/nav-topbar.php
+++ wp-content/themes/chelsea-hartman-portfolio/parts/nav-topbar.php
@@ -1,8 +1,15 @@
-<div class="top-bar" id="main-menu">
+<div class="top-bar small-bar" id="main-menu-mobile">
 	<div class="top-bar-left">
-		<ul class="menu">
-			<li><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></li>
-		</ul>
+		<a class='blog-name' href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
+	</div>
+	<div class="top-bar-right">
+		<button id="offcanvas-menu-trigger"><i class="fa fa-bars" aria-hidden="true"></i></button>
+	</div>
+</div>
+
+<div class="top-bar medium-up-bar" id="main-menu">
+	<div class="top-bar-left">
+		<a class='blog-name' href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
 	</div>
 	<div class="top-bar-right">
 		<?php joints_top_nav(); ?>
